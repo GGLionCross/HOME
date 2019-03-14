8a8ed55a1c16550282655bc800bcaa3ed0b8737d
GGLionCross <lionelquiambao@gmail.com>
2019-03-14 15:58:11 -0700

    Renamed js file for clarity of use

--- js/floating_caption.js
+++ js/floating_caption.js
@@ -1,14 +1,17 @@
+/*
+ *  This script is used to handle the floating
+ *  captions when hovering over images on the
+ *  HOME main page.
+ */
 $(document).ready(function(){ 
-    //window.alert($(this).css("height"));
     $('.expand').hover(
         function(){
             // When mouse enters
             var sibling = this.nextElementSibling; // Get the caption container
             var sib_height = sibling.clientHeight;
             var sib_width = sibling.clientWidth;
-//            $(sibling).fadeIn("slow");
             sibling.style.visibility = "visible"; // Show caption
-            //window.alert(sib_height);
+            // Make caption follow cursor
             $(document).on('mousemove', function(e){
                 $(sibling).css({
                     left: e.pageX - (sib_width / 2),
@@ -18,9 +21,7 @@ $(document).ready(function(){
         },
         function(){
             // When mouse leaves
-            //windows.alert(this.nextElementSibling.style.display);
-            this.nextElementSibling.style.visibility = "hidden"; // Hide caption
-            //$(this.nextElementSibling).fadeOut("slow");
+            this.nextElementSibling.style.visibility = "hidden";
         }
     );
 });
