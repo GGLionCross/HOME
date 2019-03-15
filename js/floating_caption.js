e1c42e913071a454b78f8b84887367112f71bc2b
GGLionCross <lionelquiambao@gmail.com>
2019-03-14 17:05:54 -0700

    Cleaned up code

--- js/floating_caption.js
+++ js/floating_caption.js
@@ -3,25 +3,22 @@
  *  captions when hovering over images on the
  *  HOME main page.
  */
-$(document).ready(function(){ 
-    $('.expand').hover(
-        function(){
-            // When mouse enters
-            var sibling = this.nextElementSibling; // Get the caption container
-            var sib_height = sibling.clientHeight;
-            var sib_width = sibling.clientWidth;
-            sibling.style.visibility = "visible"; // Show caption
-            // Make caption follow cursor
-            $(document).on('mousemove', function(e){
-                $(sibling).css({
-                    left: e.pageX - (sib_width / 2),
-                    top: e.pageY - sib_height - 50
-                });
+$(document).ready(function(){
+    $('.expand').hover(function(){
+        // When mouse enters
+        var sibling = this.nextElementSibling; // Get the caption container
+        var sib_height = sibling.clientHeight;
+        var sib_width = sibling.clientWidth;
+        sibling.style.visibility = "visible"; // Show caption
+        // Make caption follow cursor
+        $(document).on('mousemove', function(e){
+            $(sibling).css({
+                left: e.pageX - (sib_width / 2),
+                top: e.pageY - sib_height - 50
             });
-        },
-        function(){
-            // When mouse leaves
-            this.nextElementSibling.style.visibility = "hidden";
-        }
-    );
+        });
+    }, function(){
+        // When mouse leaves
+        this.nextElementSibling.style.visibility = "hidden";
+    });
 });
