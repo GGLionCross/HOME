7d4b962cf7ecf0aa86e92a7c7144f6576df8b9f4
GGLionCross <lionelquiambao@gmail.com>
2019-04-02 00:37:35 -0700

    Thickened borders, added content to main page

--- js/script.js
+++ js/script.js
@@ -8,17 +8,16 @@ $(document).ready(function(){
 	menu_btn.click(function(){
 		if (menu_flag == true){
 			menu_flag = false;
-			menu_btn.removeClass("menu-act");
-			$("#menu-btn > div").css("background-color", "white");
+			menu_btn.children("div").css("background-color", "white");
 			menu.css("border-bottom", "none");
 			menu.animate({ "margin-top": -menu_height }, "slow");
 			nav.css("box-shadow", "0 0 1px 100vmax rgba(0,0,0,0)")
 		}
 		else {
 			menu_flag = true;
-			menu_btn.addClass("menu-act");
+			menu_btn.children("div").css("background-color", "grey");
 			$("#menu-btn > div").css("background-color", "grey");
-			menu.css("border-bottom", "2px solid white");
+			menu.css("border-bottom", "3px solid white");
 			menu.animate({ "margin-top": 0 }, "slow");
 			nav.css("box-shadow", "0 0 1px 100vmax rgba(0,0,0,0.7)")
 		}
