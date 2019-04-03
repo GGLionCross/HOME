9a9aebe385f720c38e7262afb13ab71c710d67b2
GGLionCross <lionelquiambao@gmail.com>
2019-04-02 21:31:14 -0700

    Changed colors, abstracted script.js, added ability to click out of menu using a span

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
 $(document).ready(function(){
-	var nav = $("nav");
-	var menu = $("#menu-ctn");
-	var menu_btn = $("#menu-btn");
 	var menu_flag = false;
-	var menu_height = menu.height();
-	menu.css("margin-top", -menu_height);
-	menu_btn.click(function(){
+	var menu = $("#menu-ctn");
+	menu.css("margin-top",-menu.height());
+	$("#menu-btn").click(function(){
 		if (menu_flag == true){
 			menu_flag = false;
-			menu_btn.children("div").css("background-color", "white");
-			menu.css("border-bottom", "none");
-			menu.animate({ "margin-top": -menu_height }, "slow");
-			nav.css("box-shadow", "0 0 1px 100vmax rgba(0,0,0,0)")
+			menu_close();
 		}
 		else {
 			menu_flag = true;
-			menu_btn.children("div").css("background-color", "grey");
-			$("#menu-btn > div").css("background-color", "grey");
-			menu.css("border-bottom", "3px solid white");
-			menu.animate({ "margin-top": 0 }, "slow");
-			nav.css("box-shadow", "0 0 1px 100vmax rgba(0,0,0,0.7)")
+			menu_open();
 		}
 	});
+	$("#nav-span").click(function(){
+		if (menu_flag == true){
+			menu_flag = false;
+			menu_close();
+		}
+	})
 });
+
+function menu_close()
+{
+	var nav = $("nav");
+	var menu = $("#menu-ctn");
+	var menu_btn = $("#menu-btn");
+	var menu_height = menu.height();
+	menu_btn.children("div").css("background-color", "white");
+	menu.animate({ "margin-top": -menu_height }, "slow", function(){
+		// The callback function, remove border when animation finishes
+		menu.css("border-bottom", "none");
+	});
+	nav.css("box-shadow", "0 0 1px 100vmax rgba(0,0,0,0)");
+}
+
+function menu_open()
+{
+	var nav = $("nav");
+	var menu = $("#menu-ctn");
+	var menu_btn = $("#menu-btn");
+	menu_btn.children("div").css("background-color", "#DAAA00");
+	menu.css("border-bottom", "3px solid white");
+	menu.animate({ "margin-top": 0 }, "slow");
+	nav.css("box-shadow", "0 0 1px 100vmax rgba(0,0,0,0.7)");
+}
